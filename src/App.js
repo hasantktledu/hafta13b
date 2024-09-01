b5832bb6b05ce4c69d64b9c48a070bbf18254993
Hasan Tokatlı Edu <hasantktledu@gmail.com>
2024-09-01 17:00:40 +0300

    feat:Tamamlandı özelliği eklendi.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const yapilacaklar = [
+  {
+    id: 12,
+    baslik: "Yumurta alınacak",
+    tamamlandi: false
+  },
+  {
+    id: 156,
+    baslik: "Süt alınacak",
+    tamamlandi: true
+  }
+]
+
 function App() {
+  const [isler, islerGuncelle] = useState(yapilacaklar)
+
+  function tamamlamaIslemi(tiklananEleman) {
+    islerGuncelle(  eskiDeger=> {
+        return eskiDeger.map( eskiEleman=>{
+          if ( eskiEleman.id === tiklananEleman.id ) {
+            return {...eskiEleman, tamamlandi: !eskiEleman.tamamlandi}
+          } else{
+            return eskiEleman
+          }
+        } )
+      }
+     )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Yapılacaklar Listesi</h1>
+
+
+
+      {isler.map((eleman)=>{
+        return (
+          <div key={eleman.id}>
+            <label>
+              <input type='checkbox' checked={eleman.tamamlandi} onChange={ ()=>tamamlamaIslemi(eleman) } />
+              {eleman.baslik}
+            </label>
+          </div>
+        )
+      })}
+    </>
   );
 }
 
